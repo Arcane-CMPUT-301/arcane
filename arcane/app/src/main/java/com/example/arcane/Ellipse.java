7d9a64e9205e7f1c590931023361f54749fc5a8b
Tanush Das <tanushra@ualberta.ca>
2025-09-26 13:44:41 -0600

    26.1.2

--- arcane/app/src/main/java/com/example/arcane/Ellipse.java
+++ arcane/app/src/main/java/com/example/arcane/Ellipse.java
@@ -1,6 +1,6 @@
 package com.example.arcane;
 
-public class Ellipse extends Shape {
+public class Ellipse extends Shape { //new shape
     public Ellipse(int x, int y) {
         super(x, y);
     }
