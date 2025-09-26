2078f63fce315a08c0981895eda6b4a511b06475
avreet-14 <157077589+avreet-14@users.noreply.github.com>
2025-09-26 13:35:28 -0600

    Add Square

---
+++ arcane/app/src/main/java/com/example/arcane/Square.java
@@ -0,0 +1,10 @@
+package com.example.arcane;
+
+public class Square extends Shape {
+    private int side;
+
+    public Square(int x, int y, int side) {
+        super(x, y);
+        this.side = side;
+    }
+}
